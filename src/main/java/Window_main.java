da476904b269f2565c59e9ca7e41b7f5c3c3cd9f
yakov Home <yakovHome@yakov.com>
2022-07-01 00:23:26 +0300

    asd

--- src/main/java/Window_main.java
+++ src/main/java/Window_main.java
@@ -3,21 +3,20 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class Window_main extends JFrame implements SizeToApp  {
+public class Window_main extends JFrame implements SizeToApp {
 
-    Window_main()  {
-
+    Window_main() {
 
 
         //left panel
-        OriginImagePanel originImagePanel = new OriginImagePanel(0,Color.red);
+        OriginImagePanel originImagePanel = new OriginImagePanel(0, Color.red);
         this.add(originImagePanel);
         //middle panel
-        ButtonsPanel buttonsPanel = new ButtonsPanel(SCREEN_WIDTH/3,Color.YELLOW);
+        ButtonsPanel buttonsPanel = new ButtonsPanel(SCREEN_WIDTH / 3, Color.YELLOW);
         this.add(buttonsPanel);
         //right panel
-        NewImagePanel newImagePanel = new NewImagePanel(buttonsPanel.getX()+buttonsPanel.getWidth(),Color.cyan);
-this.add(newImagePanel);
+        NewImagePanel newImagePanel = new NewImagePanel(buttonsPanel.getX() + buttonsPanel.getWidth(), Color.cyan);
+        this.add(newImagePanel);
 //        // הגדרת חלון בסיסית
 
 
@@ -25,8 +24,9 @@ this.add(newImagePanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         //פונקצייה מובנית הבונה את גודל החלון בהתאם לגודל הפנלים
-        this.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
-        this.setLayout(null);this.setLocationRelativeTo(null);
+        this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
         //נראות ומיקום החלון
         this.setVisible(true);
 
@@ -34,12 +34,7 @@ this.add(newImagePanel);
     }
 
 
-
-
-
-
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.setProperty(
                 "webdriver.chrome.driver",
                 "./chromedriver102.exe");
